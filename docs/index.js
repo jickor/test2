98a6b70f6ed2e88a24a97b9bfe96ea6d2cd26c08
jickor <51739642+jickor@users.noreply.github.com>
2020-07-17 15:37:04 +0700

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,250 @@
+/*Downloaded from https://www.codeseek.co/ionelcp/random-quotes-from-the-poem-evening-star-mihai-eminescu-YpdVzM */
+ function visitPage(){
+    var searchThis = document.getElementById('getnumber').innerHTML;
+        var author = document.getElementById('aut').innerHTML;
+
+        
+   window.open('https://twitter.com/intent/tweet?text=' + searchThis + '%20' + author, '_blank');
+    }
+
+
+var quotes={
+ "1": "There was, as in the fairy tales, As ne'er in the time's raid, There was, of famous royal blood A most beautiful maid.",
+
+"2": "She was her parents' only child, Bright like the sun at noon, Like the Virgin midst the saints And among stars the moon.",
+
+"3":"From the deep shadow of the vaults Her step now she directs Toward a window- at its nook Bright Evening-star expects.",
+
+"4":"She looks as in the distant seas He rises, darts his rays And leads the blackish, loaded ships On the wet, moving, ways.",
+
+"5":"To look at him every night Her soul her instincts spur- And as he looks at her for weeks He falls in love with her.",
+
+"6":"And as on her elbows she leans Her temple and her whim She feels in her heart and soul that She falls in love with him.",
+
+"7":"And ev'ry night his stormy flames More stormily renew When in the shadow of the castle She shows to his bright view.",
+
+
+
+"8":"And to her room with her slow steps He bears his steps and aims Weaving out of his sparkles cold A toil of shaking flames.",
+
+"9":"And when she throws upon her bed Her tired limbs and reposes, He glides his light along her hands And her sweet eyelash closes.",
+
+"10":"And from the mirror on her shape A beam has spread and burns, On her big eyes that beat though closed And on her face that turns.",
+
+"11":"Her smiles view him- the mirror shows Him trembling in the nook For he is plunging in her dream So that their souls may hook.",
+
+"12":"She speaks with him in sleep and sighs While her heart's swelled veins drum: 'O sweet Lord of my fairy nights, Why comest thou not? Come !",
+
+"13":"Descend to me, mild Evening-star Thou canst glide on a beam, Enter my dwelling and my mind And over my life gleam!'",
+
+"14":"And he listens and trembles and Still more for her love craves And as quick as the lightning he Plunges into the waves.",
+
+"15":"The water in that very spot Moves rolling many rings And out of the unknown, dark, depth A superb young man springs.",
+
+"16":"As on a threshold o'er the sill His hasty steps he leads, Holds in his hand a staff with, at Its top, a crown of reeds!",
+
+"17":"A young Voivode he seems to be With soft and golden hair- A blue shroud binds in a knot on His naked shoulder fair.",
+
+"18":"The shade of his face is of wax And thou canst see throughout A handsome dead man with live eyes That throw their sparkles out.",
+
+"19":"-'From my sphere hardly I come to Follow thy call and thee, The heaven is my father and My mother is the sea.",
+
+"20":"So that I could come to thy room And look at thee from near With my light reborn from waves my Fate toward thee I steer.",
+
+"21":"O come, my treasure wonderful And thy world leave aside- For I am Evening-star up from And thou wouldst be my bride.",
+
+"22":"In my palace of coral I'll Take thee for evermore And the entire world of the sea Will kneel before thy door.'",
+
+"23":"-'O thou art beautiful as but In dreams an angel shows, The way though thou hast oped for me For me's for ever close.",
+
+"24":"Thy port and mien and speech are strange Life thy gleams don't impart, For I'm alive and thou art dead And thy eyes chill my heart.'",
+
+"25":"Days have past since: but Evening-star Comes up againd and stays Just as before, spreading o'er her His clear, translucent rays.",
+
+"26":"In sleep she would remember him And, as before, her whole Wish for the Master of the waves Is clinching now her soul.",
+
+"27":"-'Descend to me, mild Evening-star Thou canst glide on a beam, Enter my dwelling and my mind And over my life gleam!'",
+
+"28":"He hears: and from the dire despair Of such an woeful weird He dies, and the heavens revolve Where he has disappeared.",
+
+"29":"Soon in the air flames ruddy spread, The world in their grip hold- A superb form the spasms of the Chaotic valleys mold.",
+
+"30":"On his locks of black hair he bears His crown a fierce fire frames- He floats as he really comes Swimming in the sun's flames.",
+
+"31":"His black shroud lets develop out His arms marbly and hale- He pensively and sadly brings His face awfully pale.",
+
+"32":"But his big wonderful eyes' gleam, Chimerically deep, Shows two unsatiated spasms That but into dark peep.",
+
+"33":"-'From my sphere hardly I come to Follow thy voice, thy sight- The bright sun is my father and My mother is the night.",
+
+"34":"O come, my treasure wonderful And thy world leave aside For I am Evening-star from up And thou wouldst be my bride.",
+
+"35":"O come, and upon thy blond hair Crowns of stars I shall crowd, And more that all of them, up there, Thou wild look fair and proud.'",
+
+"36":"-'O thou art beautiful as but In dreams a demon shows, The way though hast oped for me For me's for ever close.",
+
+"37":"The depths of my breast ache from the Desire of thy fierce love My heavy, big eyes also ache When into them thine shove'.",
+
+"38":"-'But how wouldst thou that I come down? Know this - for, do I lie? -: I am immortal, while thou art One of those that must die!'",
+
+"39":"-'I hate big words, nor do I know How to begin my plea- And although thy discourse is clear I don't understand thee.",
+
+"40":"But if thou wantest my flamed love And that would not be sham, Come down on this temporal earth, Be mortal as I am!'",
+
+"41":"-'I'd lose my immortality For but one kiss of thine! Well, I will show thee how much too For thy fierce love I pine!",
+
+"42":"Yes, I shall be reborn from sin, Receive another creed: From that endlessness to which I Am tied, I shall be freed!'",
+
+"43":"And out he went, he went, went out, Loving a human fay, He plucked himself off from the sky, Went for many a day.",
+
+"44":"Meanwhile, the house-boy, Catalin, Sly, and who often jests When he's filling with wine the cups Of the banqueting guests-",
+
+"45":"A page that carries step by step The trail of the Queen's gown, A wandering bastard, but bold Like no one in the town-",
+
+"46":"His little cheek - a peony That under the sun stews- Watchful, just like a thief, he sneaks In Catalina's views.",
+
+"47":"-'How beautiful she grew' - thinks he - 'A flower just to pluck! Now, Catalin, but now it is Thy chance to try thy luck!'",
+
+"48":"And by the way, hurriedly, he Corners that human fay: -'What's with thee, Catalin? Let me Alone and go thy way!'",
+
+"49":"-'No! I want thee to stay away From thoughts that have no fun. I want to see thee only laugh, Give me a kiss, just one!'",
+
+"50":"-'I don't know what it is about And, believe me, retire! But for one Evening-star up from I've kept my strong desire!'",
+
+"51":"-'If thou dost not know I could show Thee all about love's balm! Only, don't give way to thy ire And listen and be calm.",
+
+"52":"So as the hunter throws the net That many birds would harm, When I'll stretch my left arm to thee, Enlace me with thy arm.",
+
+"53":"Under my eyes keep thine and don't Let them move on their wheels And if I lift thee by the waist Thou must lift on thy heels.",
+
+"54":"When I bend down my face, to hold Thine up must be thy strife- So, to each other we could throw Sweet, eager, looks for life.",
+
+"55":"And so that thou have about love A knowledge true and plain, When I stoop to kiss thee, thou must Kiss me too and again.'",
+
+"56":"With much bewilderment her mind The little boy's word fills, And shyly and nicely now she Wills not, and now she wills.",
+
+"57":"And slowly she tells him:- 'Since thy Childhood I've known thy wit, And as thou art and glib and small My temper thou wouldst fit.",
+
+"58":"But Evening-star sprung from the calm Of the oblivion, Though, gives horizon limitless To the sea lone and dun.",
+
+"59":"And secretly, I close my eyes For my eyelash tears dim When the waves of the sea go on Travelling toward him.",
+
+"60":"He shines with love unspeakable So that my pains he'd leach, But higher and higher soars, so That his hand I'd ne'er reach.",
+
+"61":"Sadly thrusts from the worlds which from My soul his cold ray bar... I shall love him for ever and For ever he'll rove far.",
+
+"62":"Like the unmeasured steppes my days Are deaf and wild, therefore, But my nights spread a holy charm I understand no more!'",
+
+"63":"-'Thou art a child! Let's go! Through new Lands our own fate let's frame! Soon they shall have lost our trace and Forgot even our name!",
+
+"64":"We shall be both wise, glad and whole As my judgement infers And thou wouldst not long for thy kin Nor yearn for Evening-stars!'",
+
+"65":"Then Evening-star went out. His wings Grow, into heavens dash, And on his way millenniums Flee in less than a flash.",
+
+"66":"Below, a depth of stars- above, The heaven stars begem, He seems an endless lightning that Is wandering through them.",
+
+"67":"And from the Chaos' vales he sees How in an immense ring Round him, as in the World's first day, Lights from their sources spring-",
+
+"68":"How, springing, they hem him like an Ocean that swimming nears... He flees carried by his desire Until he disappears.",
+
+"69":"For that region is boundless and Searching regards avoids And Time strive vainly there to come To life from the dark voids.",
+
+"70":"'Tis nought. 'Tis, though, thirst that sips him And which he cannot shun, 'Tis depth unknown, comparable To blind oblivion.",
+
+"71":"-'From that dark, choking, endlessness Into which I am furled, Father, undo me, and for e'er Be praised in the whole world!",
+
+"72":"Ask anything for this new fate For with mine I am through: O hear my prayer, O my Lord, for Thou gives life and death too.",
+
+"73":"Take back my endlessness, the fires That my being devour And in return give me a chance To love but for an hour!",
+
+"74":"I've come from Chaos- I'd return To that my former nest... And as I have been brought to life From rest, I crave for rest!'",
+
+"75":"-'Hyperion, that comest from The depths with the world's swarm, Do not ask signs and miracles That have no name nor form.",
+
+"76":"Thou wantest to count among men, Take their resemblance vain- But would now the whole mankind die Men will be born again.",
+
+"77":"But they are building on the wind Ideals void and blind- When human waves run into graves New waves spring from behind.",
+
+"78":"Fate's persecutions, lucky stars, They only are to own- Here we know neither time nor space, Death we have never known.",
+
+"79":"From the eternal yesterday Drinks what to-day will drain And if a sun dies on the sky A sun quickens again.",
+
+"80":"Risen as for ever, death though Follows them like a thorn For all are born only to die And die to be reborn.",
+
+"81":"But thou remainest wheresoe'er Thou wouldst set down or flee. Thou art of the prime form and an Eternal prodigy.",
+
+"82":"Thou wilt now hear the wondrous voice At whose bewitched singing Mounts woody get skipping to skies Into sea Island sinking!",
+
+"83":"Perhaps thou wilt more: show in deeds Thy sense of justice, might, Out of the earth's lumps make an empire And settle on its height!",
+
+"84":"I can give thee millions of vessels And hosts- thou, bear thy breath O'er all the lands, o'er all the oceans: I cannot give thee death.",
+
+"85":"For whom thou wantest then to die? Just go and see what's worth All that is waiting there for thee On that wandering earth!'",
+
+"86":"His first dominion on the sky Hyperion restores And like in his first day, his light All o'er again he pours.",
+
+"87":"For it is evening and the night Her duty never waives. Now the moon rises quietly And shaking from the waves,",
+
+"88":"And upon the paths of the groves Her sparkles again drone... Under the row of linden-trees Two youths sit all alone.",
+
+"89":"-'O darling, let my blessed ear feel How thy heart's pulses beat, Under the ray of thy eyes clear And unspeakably sweet.",
+
+"90":"With the charms of their cold light pierce My thought's faery glades, Pour an eternal quietness On my passion's dark shades.",
+
+"91":"And there, above, remain to stop Thy woe's violet stream, For thou art my first source of love And also my last dream!'",
+
+"92":"Hyperion beholds how love Their eyes equally charms: Scarcely his arm touches her neck, She takes him in her arms.",
+
+"93":"The silvery blooms spread their smells And their soft cascade strokes The tops of the heads of both youths With long and golden locks.",
+
+"94":"And all bewitched by love, she lifts Her eyes toward the fires Of the witnessing Evening-star And trusts him her desires:",
+
+"95":"-'Descend to me, mild Evening-star Thou canst glide on a beam, Enter my forest and my mind And o'er my good luck gleam!'",
+
+"96":"As he did it once, into woods, On hills, his rays he urges, Guiding throughout so many wilds The gleaming, moving, surges.",
+
+"97":"But he falls not as he did once From his height into swells: -'What matters thee, clod of dust, if 'Tis me or some one else?",
+
+"98":"You live in your sphere's narrowness And luck rules over you - But in my steady world I feel Eternal, cold and true!'"
+
+}
+
+
+var quoteNumber = Math.floor(Math.random()*(98-1+1)+1);
+
+
+
+
+
+
+$(document).ready(function(){
+    
+            $('#getnumber').html(quotes[quoteNumber]);
+        
+   
+});
+
+$(document).ready(function(){
+    $("#newq").click(function(){
+        var quoteNumber = Math.floor(Math.random()*(98-1+1)+1);
+
+        //$.get("demo_test.asp", function(data, status){
+            $('#getnumber').html(quotes[quoteNumber]);
+      var colorNumber = Math.floor(Math.random()*(6-1+1)+1);
+      if(colorNumber==1){
+      var mycolor="#D0E9FF";}//bluee
+      else if(colorNumber==2){
+      var mycolor="#FFEAEA";}//red
+      else if(colorNumber==3){
+      var mycolor="#E4FDDC";}//green
+      else if(colorNumber==4){
+       var mycolor="white";}
+      else if(colorNumber==5){
+       var mycolor="#FFFFC9";}//yellow
+      else if(colorNumber==6){
+       var mycolor="#F8DCFF";}//violet
+
+      $('.main').css( "background", mycolor );
+        //});
+    });
+});
